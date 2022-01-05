d08452b69bcb9962f5b6a289155ce15812e84325
Thais Armijos <tarmijos0985@uta.edu.ec>
2022-01-04 23:43:58 -0500

    Adicion del Login

---
+++ Project4SW/app/src/main/java/com/example/project4sw/PrincipalActivity.java
@@ -0,0 +1,30 @@
+package com.example.project4sw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PrincipalActivity extends AppCompatActivity {
+    Button boton1, boton2, boton3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+        boton1 = findViewById(R.id.btnCambioContraseña);
+        boton2 = findViewById(R.id.btnRegistrarActividad);
+        boton3 = findViewById(R.id.btnConsulta);
+
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PrincipalActivity.this, CambioContrasenaActivity.class));
+            }
+        });
+
+    }
+}
